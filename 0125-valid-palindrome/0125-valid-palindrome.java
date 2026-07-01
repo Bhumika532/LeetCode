453a3474f672c63d2778959314b6fd4a375fd717
Bhumika <bhumikathakur1948@gmail.com>
2026-07-01 17:26:01 +0530

    Time: 8 ms (37.67%), Space: 44.9 MB (43.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -10,7 +10,7 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || ch>='0' && ch<='9'){
+            if((ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
                 sb.append(ch);
             }
         }
