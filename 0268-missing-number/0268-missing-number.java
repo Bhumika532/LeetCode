09dad39255d664d36305d1abce82df0f5214e124
Bhumika <bhumikathakur1948@gmail.com>
2026-07-03 12:23:09 +0530

    Time: 106 ms (7.66%), Space: 47.3 MB (65.63%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int arrsum=0;
+        int n=nums.length;
+        for(int i=0;i<nums.length;i++){
+            arrsum+=nums[i];
+        }
+        int orgsum=n*(n+1)/2;
+        int missing=orgsum-arrsum;
+        return missing;
+    }
+}
